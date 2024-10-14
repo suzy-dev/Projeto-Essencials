d743b1f025f1b108453b3b302a2248d0573a051d
Suzana <suzanaosuza.caetano@gmail.com>
2024-10-13 21:57:30 -0300

    alterações para conectar no banco

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import connectDB from './database.js'
 import path from 'path'
 import ejs from 'ejs'
-
+import reservaRoutes from './routes/reservaRoutes.js' // Importa as rotas
 
 //config dotenv
 dotenv.config()
 
-//confg express
-const app = express()
-app.use(express.json())
+//connect MongoDB
+connectDB()
 
-//config ejs
+//config Express
+const app = express()
+app.use(express.json()) // Para que o Express possa interpretar JSON
+app.use(express.urlencoded({ extended: true })) // Para que o Express possa interpretar dados de formulários
+
+//config EJS
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-//conf mongodb
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Mongodb configurado'))
-  .catch(err => console.log(err))
-
+//rota de reserva
+app.use('/reserva', reservaRoutes); 
 
 app.get('/', (req, res) => {
     res.render('inicio.ejs')
